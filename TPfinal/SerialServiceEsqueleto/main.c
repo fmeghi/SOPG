72bc82806557c9fe9b086ff60fe4fedf401cadef
fmeghi <68160595+fmeghi@users.noreply.github.com>
2021-08-17 20:41:43 -0300

    Se agregaron comprobaciones

--- TPfinal/SerialServiceEsqueleto/main.c
+++ TPfinal/SerialServiceEsqueleto/main.c
@@ -52,8 +52,16 @@ void sigint_init(void)
     sa.sa_handler = sigint_handler;
     sa.sa_flags = 0;
     sigemptyset(&sa.sa_mask);
-    sigaction(SIGINT, &sa, NULL);
-	sigaction(SIGTERM,&sa,NULL);
+    if (sigaction(SIGINT,&sa,NULL) == -1) 
+    {
+	perror("sigaction");
+	exit(1);
+    }
+    if (sigaction(SIGTERM,&sa,NULL) == -1) 
+    {
+	perror("sigaction");
+	exit(1);
+    }
 }
 
 int socket_TCP_init(void)
